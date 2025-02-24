b268c15e8710378cafdad5bf0649b28b6ff9808b
koga002 <imkoga002@gmail.com>
2025-02-25 05:40:14 +0900

    first commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,23 @@
+use tauri::tray::TrayIconBuilder;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|app| {
+                    let tary = TrayIconBuilder::new()
+                        .icon(app.default_window_icon().unwrap().clone())
+                        .build(app)?;
+                    Ok(())
+        })
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
