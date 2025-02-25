7a4f77ddba58b7f51a3357de9606baf6e339c514
koga002 <imkoga002@gmail.com>
2025-02-25 21:26:05 +0900

    impled a rubberduck in system tray icon

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,23 +1,30 @@
-use tauri::tray::TrayIconBuilder;
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-                    let tary = TrayIconBuilder::new()
-                        .icon(app.default_window_icon().unwrap().clone())
-                        .build(app)?;
-                    Ok(())
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit_i])?;
+            let _tray = TrayIconBuilder::new()
+                .menu(&menu)
+                .menu_on_left_click(true)
+                .icon(app.default_window_icon().unwrap().clone())
+                .build(app)?;
+            Ok(())
         })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        // .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
